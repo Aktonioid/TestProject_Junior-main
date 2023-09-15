2fda7661af8778e3eac4078db6a809600b6d11b3
Павел <@DESKTOP-1ACUA2S>
2023-09-15 18:01:54 +0300

    Добавьте файлы проекта.

---
+++ ClinicApp/EntityModels/ModelMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicApp.EntityModels
+{
+    public static class ModelMapper
+    {
+        public static PatientView AsView(this PatientCard patientCard) 
+        {
+            var patientAge = DateTime.Now.Year - patientCard.DateOfBirth.Year;
+
+            if (patientCard.DateOfBirth > DateTime.Now.AddYears(-patientAge))
+            {
+                patientAge--;
+            }
+            return new PatientView
+            {
+                card = patientCard,
+                Age = patientAge
+            };
+        }
+    }
+}
---
+++ ClinicApp/EntityModels/PatientView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicApp.EntityModels
+{
+    public class PatientView
+    {
+        public PatientCard card { get; set; }
+        public int Age { get; set; }
+    }
+}
